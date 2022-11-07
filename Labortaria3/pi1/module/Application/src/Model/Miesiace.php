b529111982f5599dd2ffe947e65411a86c572a9c
Tymoteusz  Brzozowski <tybrz10@outlook.com>
2022-11-07 18:48:34 +0100

    Laboratoria
    Folder projektu PHPStorm dla lab3 z zadaniami

---
+++ Labortaria3/pi1/module/Application/src/Model/Miesiace.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\Model;
+class Miesiace
+{
+    public function pobierzWszystkie()
+    {
+        return array(
+            'red'=>'Styczeń',
+            'green'=>'Luty',
+            'blue'=>'Marzec',
+            'yellow'=>'Kwiecień',
+            'pink'=>'Maj',
+            'white'=>'Czerwiec',
+            'cyan'=>'Lipiec',
+            'grey'=>'Sierień',
+            'black'=>'Wrzesień',
+            'magenta'=>'Pażdziernik',
+            'lime'=>'Listopad',
+            'turquoise'=>'Grudzień',
+        );
+    }
+}
